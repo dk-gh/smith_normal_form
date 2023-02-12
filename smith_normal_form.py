fe4870adc63a1b1bb8470f9fd131dc3be4925113
Kevin <kevin.dunne@mailbox.org>
2023-02-12 20:06:21 +0000

    include modified version of code for sparse matrices, in particular scipy.sparse.lil_matrix

--- smith_normal_form.py
+++ smith_normal_form.py
@@ -1,5 +1,6 @@
 import numpy as np
 from itertools import product
+from scipy.sparse import lil_matrix
 
 
 def swap_rows(i0, i1, A):
@@ -11,6 +12,10 @@ def swap_columns(j0, j1, A):
 
 
 def smith_normal_form(A):
+    """
+    Input A is a numpy array with dtype=int
+    and A[i,j] = 0 or 1 for all i,j
+    """
     number_of_rows, number_of_columns = A.shape
     n = 0
     while n < min(number_of_columns, number_of_rows):
@@ -26,11 +31,43 @@ def smith_normal_form(A):
 
         for i in range(n+1, number_of_rows):
             if A[i, n] == 1:
+                # perform bitwise xor
                 A[i] = A[i] ^ A[n]
 
         for j in range(n+1, number_of_columns):
             if A[n, j] == 1:
                 A[:, [j]] = A[:, [j]] ^ A[:, [n]]
+        n = n+1
+
+    return A
+
+
+def smith_normal_form_sparse(A):
+    """
+    Input A is a lil_matrix A[i,j] = 0 or 1 for all i,j
+    """
+    number_of_rows, number_of_columns = A.shape
+    n = 0
+    while n < min(number_of_columns, number_of_rows):
+        for i, j in product(
+            range(n, number_of_rows),
+            range(n, number_of_columns)
+        ):
+
+            if A[i, j] == 1:
+                swap_rows(n, i, A)
+                swap_columns(n, j, A)
+                break
+
+        for i in range(n+1, number_of_rows):
+            if A[i, n] == 1:
+                # (x-y)**2 implements xor
+                A[i] = (A[i] - A[n]).power(2)
+
+        for j in range(n+1, number_of_columns):
+            if A[n, j] == 1:
+                # (x-y)**2 implements xor
+                A[:, [j]] = (A[:, [j]] - A[:, [n]]).power(2)
         n = n+1
 
     return A
@@ -48,5 +85,9 @@ if __name__ == '__main__':
         ]
     )
 
+    sparseX = lil_matrix(X)
+    sp_snf = smith_normal_form_sparse(sparseX)
     snf = smith_normal_form(X)
-    print(snf)
+    
+    assert (sp_snf.toarray() == snf).all()
+    
